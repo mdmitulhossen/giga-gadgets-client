da2b1d29c820365a095550d3394a379b36235404
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-10-18 12:23:56 +0600

    navbar done with dark mode toggle enable

---
+++ src/Layouts/Root.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Navber from "../components/header/Navber";
+import { useState } from "react";
+
+const Root = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className={`${darkMode ? "dark" : ""} dark:bg-[#0F172A] h-screen`}>
+
+      <Navber setDarkMode={setDarkMode} darkMode={darkMode} />
+
+      <div className="w-full h-full dark:bg-[#0F172A]">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Root;
